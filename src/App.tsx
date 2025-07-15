0fbf9ff32f5e35db45d6763e58df616a060a6fba
Kerui1023 <glds2141@leeds.ac.uk>
2025-07-15 15:38:15 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -26,9 +26,9 @@ function App() {
   return (
     <div className="chat_wrapper">
       <div className="row">
-        <span className="icon_wrapper"><img src="/后退.png" className="img" alt="后退" /></span>
+        <span className="icon_wrapper"><img src="../assets/后退.png" className="img" alt="后退" /></span>
         <span className="title">用户1</span>
-        <span className="icon_wrapper"><img src="/点.png" className="img" alt="点" /></span>
+        <span className="icon_wrapper"><img src="../assets/点.png" className="img" alt="点" /></span>
       </div>
 
       <section id="chatArea" ref={chatAreaRef}>
@@ -36,7 +36,7 @@ function App() {
       </section>
 
     <div className="input_area">
-      <img src="/语音.png" alt="语音" className="icon_img" />
+      <img src="../assets/语音.png" alt="语音" className="icon_img" />
       <input
         type="text"
         placeholder="请输入..."
@@ -46,8 +46,8 @@ function App() {
         onChange={(e) => setInput(e.target.value)}
         onKeyDown={(e) => e.key === 'Enter' && sendMessage()}
       />
-      <img src="/笑脸.png" alt="表情" className="icon_img" />
-      <img src="/加号.png" alt="更多" className="icon_img" />
+      <img src="../assets/笑脸.png" alt="表情" className="icon_img" />
+      <img src="../assets/加号.png" alt="更多" className="icon_img" />
     </div>
 
     </div>
--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,3 +1,6 @@
+import avatar1 from '../assets/1.png'
+import avatar2 from '../assets/2.png'
+
 type ChatMessageProps = {
   text: string
   from: 'left' | 'right'
@@ -7,7 +10,7 @@ const ChatMessage = ({ text, from }: ChatMessageProps) => {
   return (
     <div className={from === 'left' ? 'chat_left' : 'chat_right'}>
       <div className="avatar">
-        <img src={from === 'left' ? '/1.png' : '/2.png'} alt="头像" />
+        <img src={from === 'left' ? avatar1 : avatar2} alt="头像" />
       </div>
       <div className={from === 'left' ? 'chat_bubble_left' : 'chat_bubble_right'}>
         {text}
