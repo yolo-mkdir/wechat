e5af0205562b18c4fb76e71eee186724f04d9ab7
Kerui1023 <glds2141@leeds.ac.uk>
2025-07-15 15:59:26 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import { useEffect, useRef, useState } from 'react'
 import ChatWindow from './components/ChatWindow'
 import './style.css'
 
+import back from './assets/后退.png'
+import dot from './assets/点.png'
+import voice from './assets/语音.png'
+import face from './assets/笑脸.png'
+import plus from './assets/加号.png'
+
 function App() {
   const [messages, setMessages] = useState([
     { text: '你好呀', from: 'left' as const },
@@ -26,30 +32,29 @@ function App() {
   return (
     <div className="chat_wrapper">
       <div className="row">
-        <span className="icon_wrapper"><img src="../assets/后退.png" className="img" alt="后退" /></span>
+        <span className="icon_wrapper"><img src={back} className="img" alt="后退" /></span>
         <span className="title">用户1</span>
-        <span className="icon_wrapper"><img src="../assets/点.png" className="img" alt="点" /></span>
+        <span className="icon_wrapper"><img src={dot} className="img" alt="点" /></span>
       </div>
 
       <section id="chatArea" ref={chatAreaRef}>
         <ChatWindow messages={messages} />
       </section>
 
-    <div className="input_area">
-      <img src="../assets/语音.png" alt="语音" className="icon_img" />
-      <input
-        type="text"
-        placeholder="请输入..."
-        className="input_box"
-        id="inputBox"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        onKeyDown={(e) => e.key === 'Enter' && sendMessage()}
-      />
-      <img src="../assets/笑脸.png" alt="表情" className="icon_img" />
-      <img src="../assets/加号.png" alt="更多" className="icon_img" />
-    </div>
-
+      <div className="input_area">
+        <img src={voice} alt="语音" className="icon_img" />
+        <input
+          type="text"
+          placeholder="请输入..."
+          className="input_box"
+          id="inputBox"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => e.key === 'Enter' && sendMessage()}
+        />
+        <img src={face} alt="表情" className="icon_img" />
+        <img src={plus} alt="更多" className="icon_img" />
+      </div>
     </div>
   )
 }
