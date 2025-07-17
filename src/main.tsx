39d11f6f635b726a90275a606b6c64e89fb597dd
Kerui1023 <glds2141@leeds.ac.uk>
2025-07-17 15:36:01 +0800

    react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import router from './router'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+import App from "./App"
+import Home from "./Home"
+import ErrorPage from "./ErrorPage"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 )
-
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,10 @@
+import { Link } from 'react-router-dom'
+
 export default function Home() {
-  return <h1>这是首页</h1>
+  return (
+    <>
+      <h1>这是首页</h1>
+      <Link to="/about">去关于页</Link>
+    </>
+  )
 }
